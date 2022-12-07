b46d011ac0e3061c54e1f9ce960fe01a2ab1b21b
abdiwaliHasan <117753843+abdiwaliHasan@users.noreply.github.com>
2022-12-07 21:34:30 +0300

    introduction c++ and some Examples

---
+++ back_end/main.cpp
@@ -0,0 +1,94 @@
+#include "iostream"
+
+
+//
+//int main() {
+//    int file_size=100;
+//    int file_width=10;
+//
+//    std::cout << file_width,file_size;
+//    return 0;
+//}
+
+//using namespace std;
+//int  main() {
+//    double sales=95000;
+//    cout << "sales: $"<<sales <<endl;
+//    double state_tax=sales*.4;
+//    cout <<"state_tax: "<< state_tax <<endl;
+//    double country_tax=state_tax*.2;
+//    cout <<"country_tax:"<<country_tax<<endl;
+//    double total_sales=state_tax+country_tax;
+//    cout<<"the total is :"<<total_sales;
+//
+//    return 0;
+//}
+
+//
+//int  main(){
+//    cout <<fixed <<setprecision(2) << 12.34567;
+//
+////}
+//int main() {
+//    string name;
+//    cout << "Enter your name:";
+//
+//    getline(cin,name);
+//    cout << "hi  " <<name;
+//    return 0;
+//}
+//int main(){
+//    int x = 10;
+//    int y = 5;
+//    bool result = x !=5;
+//    cout<< result;
+//}
+//int main(){
+//    int age = 10;
+//    bool isEligibe= age>18 && age <65;
+//    cout << boolalpha <<isEligibe;
+//}
+//int main() {
+//    int temperature=70;
+//    if (temperature<60)
+//        cout<<"cold";
+//    else if (temperature < 90)
+//        cout <<"nice";
+//    return 0;
+//}
+//int main(){
+//    cout<<"1- create account" <<endl
+//    << "2- change pasword" << endl
+//    << "3- Quit"<< endl
+//    << "selct an option:";
+//
+//  short input;
+//  cin >> input;
+//    switch (input) {
+//        case 1:
+//            cout <<"you selected 1";
+//            break;
+//        case 2:
+//            cout <<"you selected 2";
+//            break;
+//        case 3:
+//            cout<<"good by";
+//    if (input==1)
+//        cout <<"you selected 1";
+//    else if (input==2)
+//        cout <<"you selected 2";
+//    else
+//        cout<<"good by";
+
+
+//using namespace std;
+//    int max (int frist, int second) {
+//        if (frist > second)
+//            return frist;
+//        else
+//            return second;
+//    }
+
+
+
+
